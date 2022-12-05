a201962cd6997057147f271ee620cde8c3c1d976
Divine Orji <dpkreativ@gmail.com>
2022-12-05 23:12:59 +0100

    Update Redux Store with Slices

--- store/store.js
+++ store/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
+import { authSlice } from './slices/authSlice';
+import { commentSlice } from './slices/commentSlice';
 
 const makeStore = () =>
   configureStore({
-    reducer: {},
+    reducer: {
+      [authSlice.name]: authSlice.reducer,
+      [commentSlice.name]: commentSlice.reducer,
+    },
     devTools: true,
   });
 
